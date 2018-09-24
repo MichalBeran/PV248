a5d5e7cafc47dc6e53ce58c743b99b1be32bdde5
MichalBeran <beran.michal@mail.muni.cz>
2018-09-24 19:30:07 +0200

    Last changes

--- 01-text/stat.py
+++ 01-text/stat.py
@@ -4,6 +4,7 @@ import sys
 import re
 from collections import Counter
 
+
 def get_ordinal_number_ending(number):
     last_digit = re.search(r"\d{1}$", number).group(0)
     if(last_digit == '1'):
@@ -15,9 +16,19 @@ def get_ordinal_number_ending(number):
     else:
         return "th"
 
+
+def get_century_from_year(year):
+    if (int(year) % 100 == 0):
+        century_year = int(year) // 100
+    else:
+        century_year = (int(year) // 100) + 1
+    return str(century_year)
+
+
 def print_collection(counter):
     for item in counter.most_common():
         print(str(item[0]) + ": "  + str(item[1]))
+
 
 def composer(file):
     counter = Counter()
@@ -28,14 +39,15 @@ def composer(file):
         if m is None:
             continue
         else:
-            splited = m.group(1).split(";")
-            for artist in splited:
+            splitted = m.group(1).split(";")
+            for artist in splitted:
                 no_brackets = re.sub(r"\(.*?\)", '', artist)
                 no_spaces = re.sub(r"\s*$", '', no_brackets)
                 no_spaces = re.sub(r"^\s*", '', no_spaces)
                 if(no_spaces != ''):
                     counter[no_spaces] += 1
     print_collection(counter)
+
 
 def century(file):
     counter = Counter()
@@ -46,20 +58,13 @@ def century(file):
         if m is None:
             continue
         else:
-            # 17th century (?<=[0-9])(?:st|nd|rd|th)  \d{1,2}(?:st|nd|rd|th)
-            # 1956 \d{4}
-            # 15.1.1960
-            # ca 1870
-            # 1732 Paris
             year = re.search(r"\d{4}", m.group(1))
             century_ordinal = re.search(r"\d{1,2}(?:st|nd|rd|th)", m.group(1))
             if year is not None:
                 year = re.match(r"(\d{1,2})(\d{2})", year.group(0))
                 if year is not None:
-                    year_century = year.group(1)
-                    if year.group(2) != "00":
-                        year_century = str(int(year_century) + 1)
-                counter[year_century + get_ordinal_number_ending(year_century) + " century"] += 1
+                    year_century = get_century_from_year(year.group(0))
+                    counter[year_century + get_ordinal_number_ending(year_century) + " century"] += 1
             elif century_ordinal is not None:
                 counter[century_ordinal.group(0) + " century"] += 1
     print_collection(counter)
