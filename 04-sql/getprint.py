7b78b2472d00f94486623ce8ce0b6e8c79647dbd
Michal Beran <beran.mi.g@gmail.com>
2018-10-22 18:57:06 +0200

    04 sql implementation (#13)
    * 04 hw init
    
    * 04 implementation
    
    * Removed dat file from git.
    
    * Changed output format, fixed bugs

---
+++ 04-sql/getprint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import sys
+import sqlite3
+import json
+
+def search_composers(print_id):
+    conn = sqlite3.connect('scorelib.dat')
+    # conn.set_trace_callback(print)
+    cur = conn.cursor()
+    res = cur.execute('SELECT person.name, person.born, person.died FROM person '
+                      'JOIN score_author on person.id = score_author.composer '
+                      'JOIN edition on edition.score = score_author.score '
+                      'JOIN print on edition.id = print.edition WHERE print.id = ?',
+                      (print_id,)).fetchall()
+    items = []
+    if res is not None:
+        for person in res:
+            item = {}
+            item["name"] = person[0]
+            if person[1] is not None:
+                item["born"] = person[1]
+            if person[2] is not None:
+                item["died"] = person[2]
+            items.append(item)
+    print(json.dumps(items, ensure_ascii=False, sort_keys=True, indent=4))
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 2:
+        search_composers(sys.argv[1])
+    else:
+        print("Wrong number of arguments")
