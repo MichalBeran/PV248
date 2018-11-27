ea9f406f1f4e6c88985dbc9d36d6150d1983a3d8
MichalBeran <beran.michal@mail.muni.cz>
2018-11-27 20:12:24 +0100

    SSL check changed

--- 09-http/http-forward.py
+++ 09-http/http-forward.py
@@ -23,46 +23,55 @@ def return_invalid_json():
 def dict_parser(multi_dict):
     result = {}
     for k, v in multi_dict.items():
-        result[str(k)] = str(v)
+        if str(k) in result.keys():
+            result[str(k)] += ', ' + str(v)
+        else:
+            result[str(k)] = str(v)
     return result
 
 
 def get_url(url_string, default_method='http'):
-    https = False
     result = urlparse(url_string, scheme=default_method)
     if result.netloc == '':
         result = urlparse('//' + url_string, scheme=default_method)
-    if result.scheme == 'https':
-        https = True
-        print('HTTPS')
-    return result.geturl(), https
+    return result.geturl()
 
 
-async def check_ssl(url_host, url_port, timeout):
+# check if invalid or no cert at all - ex http
+async def check_ssl(url_host, url_port):
+    print('ssl port', url_port)
     res_json = {}
     ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_REQUIRED
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(timeout)
-    wrappedSocket = ssl_context.wrap_socket(sock)
+    # ssl_context.check_hostname = False
+    # ssl_context.verify_mode = ssl.CERT_REQUIRED
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # sock.settimeout(timeout)
+    wrappedSocket = ssl_context.wrap_socket(socket.socket(), server_hostname=url_host)
     try:
         wrappedSocket.connect((url_host, url_port))
-    except:
+    except ssl.CertificateError:
+        print('SSL CERT ERROR')
         res_json['certificate valid'] = False
-    else:
-        pem_cert = ssl.DER_cert_to_PEM_cert(wrappedSocket.getpeercert(True))
-        cert = wrappedSocket.getpeercert()
-        subject = dict(x[0] for x in cert['subject'])
-        issued_to = subject['commonName']
-        print('issued to', issued_to)
-        res_json['certificate valid'] = True
-        res_json['certificate for'] = issued_to
+    except ssl.SSLError as e:
+        print('SSL ERROR', e.filename, '|', e.filename2, '|', e.library, '|', e.strerror)
+        print(type(e).__name__,)
+        pass
+    except Exception as e:
+        print('unknown ERROR', e)
+        pass
+
+    cert = wrappedSocket.getpeercert()
+    issued_to = []
+    for altName in cert['subjectAltName']:
+        issued_to.append(altName[1])
+    print('issued to', issued_to)
+    res_json['certificate valid'] = True
+    res_json['certificate for'] = issued_to
     wrappedSocket.close()
     return res_json
 
 
-async def get_request(url, headers, req_timeout, verify_ssl):
+async def get_request(url, headers, req_timeout):
     res_json = {}
     conn = aiohttp.TCPConnector(verify_ssl=False)
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=req_timeout), connector=conn) as session:
@@ -78,16 +87,18 @@ async def get_request(url, headers, req_timeout, verify_ssl):
                     pass
                 if 'json' not in res_json.keys():
                     res_json['content'] = await resp.text()
-                if verify_ssl:
-                    ssl_json = await check_ssl(resp.url.host, resp.url.port, default_timeout)
+                try:
+                    ssl_json = await check_ssl(resp.url.host, resp.url.port)
                     for key, value in ssl_json.items():
                         res_json[key] = value
+                except Exception:
+                    pass
         except asyncio.TimeoutError:
             res_json['code'] = 'timeout'
     return web.json_response(data=res_json)
 
 
-async def post_request(url, headers, req_timeout, data_content, verify_ssl):
+async def post_request(url, headers, req_timeout, data_content):
     res_json = {}
     conn = aiohttp.TCPConnector(verify_ssl=False)
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=req_timeout), connector=conn) as session:
@@ -104,10 +115,12 @@ async def post_request(url, headers, req_timeout, data_content, verify_ssl):
                     pass
                 if 'json' not in res_json.keys():
                     res_json['content'] = await resp.text()
-                if verify_ssl:
-                    ssl_json = await check_ssl(resp.url.host, resp.url.port, default_timeout)
+                try:
+                    ssl_json = await check_ssl(resp.url.host, resp.url.port)
                     for key, value in ssl_json.items():
                         res_json[key] = value
+                except Exception:
+                    pass
         except asyncio.TimeoutError:
             res_json['code'] = 'timeout'
     return web.json_response(data=res_json)
@@ -123,7 +136,7 @@ async def handle_get(request):
     print('headers', result)
     url, https = get_url(upstream)
     print('url:', url)
-    response_result = await get_request(url=url, headers=result, req_timeout=default_timeout, verify_ssl=False)
+    response_result = await get_request(url=url, headers=result, req_timeout=default_timeout)
     return response_result
 
 
@@ -135,7 +148,7 @@ async def handle_post(request):
     con = (await request.read()).decode()
     try:
         json_content = json.loads(con)
-    except json.decoder.JSONDecodeError:
+    except:
         return return_invalid_json()
 
     if "type" not in json_content:
@@ -158,12 +171,12 @@ async def handle_post(request):
         timeout = json_content['timeout']
     print('content url', json_content['url'])
     print(await request.read())
-    url, verify_ssl = get_url(json_content['url'])
+    url = get_url(json_content['url'])
     print("parsed url", url)
     if json_content['type'] == 'GET':
-        return await get_request(url, headers, timeout, verify_ssl=verify_ssl)
+        return await get_request(url, headers, timeout)
     elif json_content['type'] == 'POST':
-        return await post_request(url, headers, timeout, json_content['content'], verify_ssl=verify_ssl)
+        return await post_request(url, headers, timeout, json_content['content'])
     else:
         return web.Response(text="INVALID REQUEST", status=400)
 
