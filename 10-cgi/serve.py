0017ef4af464e47e1327356e76b3f06fe5c560e2
MichalBeran <beran.michal@mail.muni.cz>
2018-12-12 10:06:57 +0100

    Fixed env variables

--- 10-cgi/serve.py
+++ 10-cgi/serve.py
@@ -14,11 +14,36 @@ async def handle_get_request(request):
     # asyncio.create_subprocess_shell()
     # os.popen()
     path = request.path
-    query = request.query_string
-    os.putenv("QUERY_STRING", str(query))
     system_path = directory + path
     if os.path.isfile(system_path):
         if system_path.endswith(".cgi"):
+            if request.headers.get("Authorization") is None:
+                auth_type = ''
+            else:
+                auth_type = request.headers.get("Authorization")
+
+            if request.content_type is None:
+                content_type = ''
+            else:
+                content_type = request.content_type
+            os.putenv('AUTH_TYPE', auth_type)
+            os.putenv('CONTENT_TYPE', content_type)
+            os.putenv('GATEWAY_INTERFACE', 'CGI/1.1')
+            os.putenv('PATH_INFO', request.url.raw_path)
+            os.putenv('QUERY_STRING', request.query_string)
+            os.putenv('REMOTE_ADDR', '127.0.0.1')
+            os.putenv("REQUEST_METHOD", request.method)
+            os.putenv("SERVER_NAME", '127.0.0.1')
+            os.putenv("SERVER_PORT", str(request.PORT))
+            os.putenv("SERVER_PROTOCOL", 'HTTP/1.1')
+            software = 'Python ' + str(sys.version_info[0]) + '.' + str(sys.version_info[1]) + '.' + str(
+                sys.version_info[2])
+            os.putenv("SCRIPT_NAME", os.path.basename(str(request.url)))
+            os.putenv("SERVER_SOFTWARE", software)
+            if request.content_length is not None:
+                os.putenv('CONTENT_LENGTH', str(request.content_length))
+            else:
+                os.putenv('CONTENT_LENGTH', str(0))
             data = await asyncio.create_subprocess_shell(system_path, stdout=asyncio.subprocess.PIPE)
             output = await data.stdout.read()
             parsed = output.decode().split("\n\n")
@@ -40,6 +65,33 @@ async def handle_post_request(request):
     system_path = directory + path
     if os.path.isfile(system_path):
         if system_path.endswith(".cgi"):
+            if request.headers.get("Authorization") is None:
+                auth_type = ''
+            else:
+                auth_type = request.headers.get("Authorization")
+
+            if request.content_type is None:
+                content_type = ''
+            else:
+                content_type = request.content_type
+            os.putenv('AUTH_TYPE', auth_type)
+            os.putenv('CONTENT_TYPE', content_type)
+            os.putenv('GATEWAY_INTERFACE', 'CGI/1.1')
+            os.putenv('PATH_INFO', request.url.raw_path)
+            os.putenv('QUERY_STRING', request.query_string)
+            os.putenv('REMOTE_ADDR', '127.0.0.1')
+            os.putenv("REQUEST_METHOD", request.method)
+            os.putenv("SERVER_NAME", '127.0.0.1')
+            os.putenv("SERVER_PORT", str(request.PORT))
+            os.putenv("SERVER_PROTOCOL", 'HTTP/1.1')
+            software = 'Python ' + str(sys.version_info[0]) + '.' + str(sys.version_info[1]) + '.' + str(
+                sys.version_info[2])
+            os.putenv("SCRIPT_NAME", os.path.basename(str(request.url)))
+            os.putenv("SERVER_SOFTWARE", software)
+            if request.content_length is not None:
+                os.putenv('CONTENT_LENGTH', str(request.content_length))
+            else:
+                os.putenv('CONTENT_LENGTH', str(0))
             data = await asyncio.create_subprocess_shell(system_path, stdin=body, stdout=asyncio.subprocess.PIPE)
             output = await data.stdout.read()
             parsed = output.decode().split("\n\n")
